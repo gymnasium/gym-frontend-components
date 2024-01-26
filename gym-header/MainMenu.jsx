0a91f738bacea443d573bf9fd95c74140561e513
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-26 11:22:45 -0500

    Set active state when appropriate

--- gym-header/MainMenu.jsx
+++ gym-header/MainMenu.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import GymSettings from '../settings';
 const settings = await GymSettings();
 
-const MainMenu = ({}) => {
+const MainMenu = ({
+  secondaryNav,
+}) => {
   return ( 
     <ul role="list">
       {settings.navigation.main.map((item, index) => {
+        let activeAria;
+        let activeClass;
+        if (secondaryNav && item.href.includes(secondaryNav)) {
+          activeAria = 'page';
+          activeClass = 'active';
+        };
         return (
-          <li key={`main-item-${index}`}><a href={item.href}>{item.title}</a></li>
+          <li key={`main-item-${index}`}><a href={item.href} aria-current={activeAria} className={activeClass}>{item.title}</a></li>
         );
       })}
     </ul>
   )
-};
-
-MainMenu.propTypes = {
-
 };
 
 export default MainMenu;
