768c6f3c95347ac03d68ea5f1709500ca412dfb0
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-26 11:22:06 -0500

    use MARKETING_SITE_BASE_URL

--- settings.jsx
+++ settings.jsx
@@ -1,11 +1,12 @@
 import { getConfig } from '@edx/frontend-platform';
 const config = getConfig();
+
 const root = config.MARKETING_SITE_BASE_URL;
+console.log(`settings:`, config);
 
 export default async function Settings() {
   try {
-    let response = await fetch(`http://gym.soy/feeds/config.json`);
-    // let response = await fetch(`https://gym.soy/feeds/config.json`);
+    let response = await fetch(`${root}/feeds/config.json`);
     let responseJson = await response.json();
     return responseJson;
    } catch(error) {
