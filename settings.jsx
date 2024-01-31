e942fe58fe54733b3e9e7658e9ba9af378f9b845
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-31 16:58:56 -0500

    add console.log for debugging

--- settings.jsx
+++ settings.jsx
@@ -1,13 +1,14 @@
 import { getConfig } from '@edx/frontend-platform';
 const config = getConfig();
-
+console.log(`config:`, config);
 const root = config.MARKETING_SITE_BASE_URL;
-console.log(`settings:`, config);
 
 export default async function Settings() {
   try {
     let response = await fetch(`${root}/feeds/config.json`);
     let responseJson = await response.json();
+
+    console.log(`settings: `, responseJson);
     return responseJson;
    } catch(error) {
     console.error(error);
