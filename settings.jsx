40dc89a3c81500bbf01634cc93811046dbb2b3a8
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-04-02 15:56:05 -0400

    add logging, update version

--- settings.jsx
+++ settings.jsx
@@ -1,6 +1,6 @@
 import { ensureConfig, getConfig } from '@edx/frontend-platform';
 
-ensureConfig(['MARKETING_SITE_BASE_URL'],'gym-frontend-components');
+ensureConfig(['MARKETING_SITE_BASE_URL'],'Settings()');
 
 const config = getConfig();
 console.log(`config:`, config);
@@ -15,6 +15,12 @@ if (typeof config.MARKETING_SITE_BASE_URL !== 'undefined' && config.MARKETING_SI
   // probably want to avoid hard-coding this, but this is here as a failsafe just in case all of the above fails.
   root = 'http://edly.io:8888';
 }
+
+console.log(
+  `root: `, root,
+  `config.MARKETING_SITE_BASE_URL: `, config.MARKETING_SITE_BASE_URL,
+  `process.env.MARKETING_SITE_BASE_URL: `, process.env.MARKETING_SITE_BASE_URL
+);
 
 export default async function Settings() {
   try {
