3e4a63d0ee9ceb531c5799fa810b6ed21af66d73
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-04-24 10:26:19 -0400

    Remove GymSettings completely

--- index.jsx
+++ index.jsx
@@ -1,8 +1,5 @@
 import GymFooter from './gym-footer/GymFooter';
 import GymHeader from './gym-header/GymHeader';
 import { htmlDecode, slugify, timestamp } from './helpers';
-import Settings from './settings';
 
-const GymSettings = await Settings();
-
-export { GymFooter, GymHeader, GymSettings, htmlDecode, slugify, timestamp };
+export { GymFooter, GymHeader, htmlDecode, slugify, timestamp };
