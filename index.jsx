edfbfe246a37c423b0ab12c5961d39f42bedb0be
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-08-06 09:51:58 -0400

    Add GymMeta

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import GymFooter from './gym-footer/GymFooter';
 import GymHeader from './gym-header/GymHeader';
+import GymMeta from './gym-meta/GymMeta';
 import { htmlDecode, slugify, timestamp } from './helpers';
 
-export { GymFooter, GymHeader, htmlDecode, slugify, timestamp };
+export { GymFooter, GymHeader, GymMeta, htmlDecode, slugify, timestamp };
