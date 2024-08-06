036bc0fe21454372a5b2958596cbab4ab7ce5eb3
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-08-06 12:54:33 -0400

    Update GymFooter.jsx

--- gym-footer/GymFooter.jsx
+++ gym-footer/GymFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ensureConfig, getConfig } from '@edx/frontend-platform';
 import PropTypes from 'prop-types';
 import { sendTrackEvent } from '@edx/frontend-platform/analytics';
