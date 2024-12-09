75d4f37655cf0456ba791796a51707a999b7a60d
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-12-09 16:02:50 -0500

    feat: add Learner Dashboard components

--- index.jsx
+++ index.jsx
@@ -2,7 +2,7 @@ import GymErrors from './errors/GymErrors';
 import GymFooter from './footer/GymFooter';
 import GymHeader from './header/GymHeader';
 import GymMeta from './meta/GymMeta';
-import ErrorPage from './overrides/';
+import { LearnerDashboard, ErrorPage } from './overrides/';
 import { htmlDecode, slugify, timestamp } from './helpers';
 
-export { ErrorPage, GymFooter, GymHeader, GymMeta, GymErrors, htmlDecode, slugify, timestamp };
+export { ErrorPage, GymFooter, GymHeader, GymMeta, GymErrors, htmlDecode, LearnerDashboard, slugify, timestamp };
--- overrides/index.js
+++ overrides/index.js
@@ -1,1 +1,2 @@
-export { default } from './ErrorPage';
+export { default as ErrorPage } from './ErrorPage';
+export { default as LearnerDashboard } from './learner-dashboard';
