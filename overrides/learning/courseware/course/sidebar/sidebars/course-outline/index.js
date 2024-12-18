7c97381d12b94758eca597437ee3295b101142b9
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-12-18 15:52:10 -0500

    chore: add latest? defaults with updated paths

---
+++ overrides/learning/courseware/course/sidebar/sidebars/course-outline/index.js
@@ -0,0 +1,3 @@
+export { default as Sidebar } from './CourseOutlineTray';
+export { default as Trigger } from './CourseOutlineTrigger';
+export { ID } from '@src/courseware/course/sidebar/sidebars/course-outline/constants';
