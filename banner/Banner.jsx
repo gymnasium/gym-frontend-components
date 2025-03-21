2d4920a95206b7c67a02ac87b670d395ea2d3cf1
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-03-21 15:15:05 -0400

    feat: add Banner component

---
+++ banner/Banner.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { ensureConfig, getConfig } from '@edx/frontend-platform';
+import { htmlDecode } from '../helpers';
+import dompurify from 'dompurify';
+
+ensureConfig(['STATIC_ASSETS_URL'], 'Banner');
+
+const STATIC_ASSETS_URL = () => getConfig().STATIC_ASSETS_URL;
+const API_ENDPOINT = () => `${STATIC_ASSETS_URL()}/api/config.json`;
+
+function Banner() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(API_ENDPOINT());
+        if (!response.ok) {
+          setError(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading data…</p>;
+  }
+
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  }
+  
+  const showBanner = data && data?.msg?.banner?.active;
+  const banner = showBanner && htmlDecode(data?.msg?.banner?.content);
+  const bannerClasses = showBanner && ['site-status', data?.msg?.banner?.class];
+  const sanitizedBanner = { __html: dompurify.sanitize(banner) };
+
+  return (
+    <aside id="site-status" className={bannerClasses.join(' ')}>
+      <div className="container" dangerouslySetInnerHTML={ sanitizedBanner } />
+    </aside>
+  );
+}
+
+export default Banner;
--- index.jsx
+++ index.jsx
@@ -1,8 +1,9 @@
 import GymErrors from './errors/GymErrors';
 import GymFooter from './footer/GymFooter';
 import GymHeader from './header/GymHeader';
+import Banner from './banner/Banner';
 import GymMeta from './meta/GymMeta';
 import { LearnerDashboard, ErrorPage } from './overrides/';
 import { htmlDecode, slugify, timestamp } from './helpers';
 
-export { ErrorPage, GymFooter, GymHeader, GymMeta, GymErrors, htmlDecode, slugify, timestamp };
+export { Banner, ErrorPage, GymFooter, GymFooter as Footer, GymHeader, GymHeader as Header, GymMeta, GymErrors, htmlDecode, slugify, timestamp };
