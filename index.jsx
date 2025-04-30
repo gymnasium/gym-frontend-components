6729d7278b10a36fcb015d07696902ad79f10cf5
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-04-30 10:36:59 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,9 +1,18 @@
 import GymErrors from './errors/GymErrors';
 import GymFooter from './footer/GymFooter';
 import GymHeader from './header/GymHeader';
-import Banner from './banner/Banner';
-import GymMeta from './meta/GymMeta';
 import { LearnerDashboard, ErrorPage } from './overrides/';
 import { htmlDecode, slugify, timestamp } from './helpers';
 
-export { Banner, ErrorPage, GymFooter, GymFooter as Footer, GymHeader, GymHeader as Header, GymMeta, GymErrors, htmlDecode, slugify, timestamp };
+export { 
+  ErrorPage,
+  GymFooter,
+  GymFooter as Footer,
+  GymHeader,
+  GymHeader as Header,
+  GymErrors,
+  GymErrors as Errors,
+  htmlDecode,
+  slugify,
+  timestamp,
+};
