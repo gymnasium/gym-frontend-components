1bc3c3532b888cc637dae7caef24f3a36a9bfaa1
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-05-21 09:40:18 -0400

    feat: disable registration button properly

--- header/AnonUserMenu.jsx
+++ header/AnonUserMenu.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { ensureConfig, getConfig } from '@edx/frontend-platform';
 import { getLoginRedirectUrl } from '@edx/frontend-platform/auth';
 
-ensureConfig(['LMS_BASE_URL','GYM_LOGIN_LABEL', 'GYM_REGISTER_LABEL'], 'AnonUserMenu');
+ensureConfig(['LMS_BASE_URL','GYM_LOGIN_LABEL', 'GYM_REGISTER_LABEL', 'ALLOW_PUBLIC_ACCOUNT_CREATION', 'SHOW_REGISTRATION_LINKS'], 'AnonUserMenu');
 
 const getLmsUrl = () => getConfig().LMS_BASE_URL;
 const getLoginLabel = () => getConfig().GYM_LOGIN_LABEL;
 const getRegisterLabel = () => getConfig().GYM_REGISTER_LABEL;
+const showRegistration = () => getConfig().ALLOW_PUBLIC_ACCOUNT_CREATION === true || getConfig().SHOW_REGISTRATION_LINKS === true;
 
 const AnonUserMenu = ({ }) => (
   <ul className="auth" role="list">
@@ -16,11 +17,11 @@ const AnonUserMenu = ({ }) => (
     >
       {getLoginLabel()}
     </a></li>
-    <li key="unauth-item-2"><a className="btn"
+    {showRegistration() && <li key="unauth-item-2"><a className="btn"
       href={`${getLmsUrl()}/register?next=${encodeURIComponent(global.location.href)}`}
     >
       {getRegisterLabel()}
-    </a></li>
+    </a></li>}
   </ul>
 );
 
